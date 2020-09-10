87662dc26e01e584e2f728ca625010b4323fbc7a
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-10 23:52:04 +0900

    add: Paramiko sample code

---
+++ paramiko/sample.py
@@ -0,0 +1,12 @@
+import paramiko
+
+# 接続
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.connect("192.168.0.20", username='zaki', key_filename="/home/zaki/.ssh/id_rsa_nopass")
+
+stdin, stdout, stderr = client.exec_command("hostname")
+for line in stdout:
+    print(line)
+
+client.close()
