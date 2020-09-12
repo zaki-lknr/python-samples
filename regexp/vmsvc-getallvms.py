a1f3ae593466e5b18f17f4d7ecbada73dfba774f
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-12 23:32:37 +0900

    add: データをdictionaryにセット

--- regexp/vmsvc-getallvms.py
+++ regexp/vmsvc-getallvms.py
@@ -7,6 +7,8 @@ if __name__ == '__main__':
 
     pattern = '(\d+)\s+(.+)\s+\[(.+)\] (.+\.vmx)\s+(\S+)\s+(\S+)'
 
+    vmlist = {}
+
     f = open("vmsvc-getallvms.txt")
     for line in iter(f.readline, ''):
         # print(line.rstrip())
@@ -19,6 +21,15 @@ if __name__ == '__main__':
             print("file: " + result.group(4))
             print("guest: " + result.group(5))
             print("version: " + result.group(6))
-
+            vmlist[result.group(2)] = {
+                "vmid": result.group(1),
+                "ds": result.group(3),
+                "file": result.group(4),
+                "guest": result.group(5),
+                "version": result.group(6)
+            }
 
     f.close()
+
+    print(vmlist)
+    # print(vmlist["cloud-dev"]["vmid"])
