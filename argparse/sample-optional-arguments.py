0ce3a196c836f2eb9cd7152fa354fddb78eac1b5
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-14 08:08:11 +0900

    add: Argparse: optional arguments フラグ値

--- argparse/sample-optional-arguments.py
+++ argparse/sample-optional-arguments.py
@@ -8,10 +8,13 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbosity", help="increase output verbosity")
+    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
     if args.verbosity:
         print("verbosity turned on")
+    if args.verbose:
+        print("verbose turned on")
 
 
     # 上のコードで下記のようにオプション/引数指定で実行。
@@ -27,14 +30,23 @@ if __name__ == '__main__':
     (paramiko) [zaki@cloud-dev argparse]$ 
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py 
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --help
-    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY]
+    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [--verbose]
 
     optional arguments:
     -h, --help            show this help message and exit
     --verbosity VERBOSITY
                             increase output verbosity
+    --verbose             increase output verbosity
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbosity
     usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY]
     sample-optional-arguments.py: error: argument --verbosity: expected one argument
     (paramiko) [zaki@cloud-dev argparse]$ 
+    (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbose
+    verbose turned on
+    (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbose 1
+    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [--verbose]
+    sample-optional-arguments.py: error: unrecognized arguments: 1
+    (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbose true
+    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [--verbose]
+    sample-optional-arguments.py: error: unrecognized arguments: true
     """
