60cb9f8d2f71704f19e012ef6a9cbde9dbd6191a
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-13 21:30:35 +0900

    add: ArgumentParser: type=int型指定

--- argparse/sample.py
+++ argparse/sample.py
@@ -8,27 +8,35 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("echo", help="echo the string you use here")
+    parser.add_argument("square", help="display a square of a given number", type=int)
     args = parser.parse_args()
 
     print(args.echo)
+    print(args.square**2)
 
     # 上のコードで下記のようにオプション/引数指定で実行
     # 引数(echo)が必須になり、未指定時にはエラー(簡易メッセージ)となる。
     # -hで詳しいメッセージが出力される。
     # その際は`add_argument()`の引数にhelp=(string)でセットしたものが表示される。
+    # 更にtype=intを指定すると、integerとして扱われる。
 
     """
     (paramiko) [zaki@cloud-dev argparse]$ python sample.py 
-    usage: sample.py [-h] echo
-    sample.py: error: the following arguments are required: echo
+    usage: sample.py [-h] echo square
+    sample.py: error: the following arguments are required: echo, square
     (paramiko) [zaki@cloud-dev argparse]$ python sample.py -h
-    usage: sample.py [-h] echo
+    usage: sample.py [-h] echo square
 
     positional arguments:
     echo        echo the string you use here
+    square      display a square of a given number
 
     optional arguments:
     -h, --help  show this help message and exit
-    (paramiko) [zaki@cloud-dev argparse]$ python sample.py hoge
+    (paramiko) [zaki@cloud-dev argparse]$ python sample.py hoge 4
     hoge
+    16
+    (paramiko) [zaki@cloud-dev argparse]$ python sample.py hoge four
+    usage: sample.py [-h] echo square
+    sample.py: error: argument square: invalid int value: 'four'
     """
