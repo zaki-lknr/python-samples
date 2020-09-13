c02c8bee823e6c22f2b15a7b290444dd2aa9ebb8
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-13 20:59:56 +0900

    add: parser.add_argument(echo)で必須の引数(positional arguments)を設定

--- argparse/sample.py
+++ argparse/sample.py
@@ -7,24 +7,27 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser.add_argument("echo")
+    args = parser.parse_args()
+
+    print(args.echo)
 
     # 上のコードで下記のようにオプション/引数指定で実行
+    # 引数(echo)が必須になり、未指定時にはエラー(簡易メッセージ)となる。
+    # -hで詳しいメッセージが出力される
 
     """
-    (paramiko) [zaki@cloud-dev argparse]$ python sample.py --help
-    usage: sample.py [-h]
-    
+    (paramiko) [zaki@cloud-dev argparse]$ python sample.py 
+    usage: sample.py [-h] echo
+    sample.py: error: the following arguments are required: echo
+    (paramiko) [zaki@cloud-dev argparse]$ python sample.py -h
+    usage: sample.py [-h] echo
+
+    positional arguments:
+    echo
+
     optional arguments:
-      -h, --help  show this help message and exit
-    (paramiko) [zaki@cloud-dev argparse]$ python sample.py --version
-    usage: sample.py [-h]
-    sample.py: error: unrecognized arguments: --version
-    (paramiko) [zaki@cloud-dev argparse]$ python sample.py --verbose
-    usage: sample.py [-h]
-    sample.py: error: unrecognized arguments: --verbose
+    -h, --help  show this help message and exit
     (paramiko) [zaki@cloud-dev argparse]$ python sample.py hoge
-    usage: sample.py [-h]
-    sample.py: error: unrecognized arguments: hoge
-    (paramiko) [zaki@cloud-dev argparse]$ 
+    hoge
     """
