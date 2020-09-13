71d8d385d919c2b2e2657898e84b950f0007bc7e
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-14 08:35:21 +0900

    add: Argparse: optional arguments: ショートオプション

--- argparse/sample-optional-arguments.py
+++ argparse/sample-optional-arguments.py
@@ -8,7 +8,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--verbosity", help="increase output verbosity")
-    parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
     if args.verbosity:
@@ -30,13 +30,13 @@ if __name__ == '__main__':
     (paramiko) [zaki@cloud-dev argparse]$ 
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py 
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --help
-    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [--verbose]
+    usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [-v]
 
     optional arguments:
     -h, --help            show this help message and exit
     --verbosity VERBOSITY
                             increase output verbosity
-    --verbose             increase output verbosity
+    -v, --verbose         increase output verbosity
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbosity
     usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY]
     sample-optional-arguments.py: error: argument --verbosity: expected one argument
@@ -49,4 +49,6 @@ if __name__ == '__main__':
     (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py --verbose true
     usage: sample-optional-arguments.py [-h] [--verbosity VERBOSITY] [--verbose]
     sample-optional-arguments.py: error: unrecognized arguments: true
+    (paramiko) [zaki@cloud-dev argparse]$ python sample-optional-arguments.py -v
+    verbose turned on
     """
