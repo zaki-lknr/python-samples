0c17ac767abf703ac27a7fc03265ef2f58356718
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-14 23:50:37 +0900

    add: configparserお試し (python2のコードからコピー)

---
+++ configparser/sample.py
@@ -0,0 +1,9 @@
+import configparser
+
+ini = configparser.SafeConfigParser()
+ini.read('sample.ini')
+interpreter = ini.get('defaults', 'interpreter_python')
+ssh_config = ini.get('ssh_connection', 'ssh_args')
+
+print(interpreter)
+print(ssh_config)
