322d9a658507fce2b7f64125ad33195089ae1867
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-17 08:29:26 +0900

    add: classの継承

--- class/sample.py
+++ class/sample.py
@@ -8,7 +8,22 @@ class Animal:
         else:
             print(self.nakigoe)
 
+class Cat(Animal):
+    def __init__(self, a=None):
+        if a:
+            self.nakigoe = a
+        else:
+            self.nakigoe = "nya-n"
+    # def naku(self, arg=None):
+    #     if arg:
+    #         print(arg)
+    #     else:
+    #         print("nya-n")
+
 neko = Animal("nya-n")
 neko.naku()
 neko.naku("wanwan")
 neko.naku()
+
+cat = Cat()
+cat.naku()
