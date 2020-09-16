eba2fbbb1c928d5d98cc4f2c54ecb8664ed3d13b
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-16 12:50:10 +0900

    update: vmsvc/getallvms実行部分を関数化

--- esxi-ssh/getallvms.py
+++ esxi-ssh/getallvms.py
@@ -1,6 +1,16 @@
 import configparser
 import paramiko
 
+def get_all_vms(ssh_client):
+    stdin, stdout, stderr = client.exec_command("vim-cmd vmsvc/getallvms")
+    for line in stdout:
+        print(line.rstrip())
+    print("\n")
+
+    stdin.close()
+    stdout.close()
+    stderr.close()
+
 ini = configparser.SafeConfigParser()
 ini.read('authenticate.conf')
 esxihost = ini.get('esxi', 'host')
@@ -14,13 +24,6 @@ client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.connect(esxihost, username=username, password=password)
 
-stdin, stdout, stderr = client.exec_command("vim-cmd vmsvc/getallvms")
-for line in stdout:
-    print(line.rstrip())
-print("\n")
-
-stdin.close()
-stdout.close()
-stderr.close()
+get_all_vms(client)
 
 client.close()
