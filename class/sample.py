332f7b5e95123fe18a2b3281930f5b5c9092680e
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-16 22:28:36 +0900

    add: Classお試し

---
+++ class/sample.py
@@ -0,0 +1,11 @@
+class Animal:
+    nakigoe = ""
+    def set_nakigoe(self, a):
+        self.nakigoe = a
+
+    def naku(self):
+        print(self.nakigoe)
+
+neko = Animal()
+neko.set_nakigoe("nya-n")
+neko.naku()
