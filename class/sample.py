5c164d7b6c68c269ed31b316117512c498eef5aa
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-17 00:13:39 +0900

    add: デフォルト引数で疑似オーバーロード

--- class/sample.py
+++ class/sample.py
@@ -2,8 +2,13 @@ class Animal:
     def __init__(self, a):
         self.nakigoe = a
 
-    def naku(self):
-        print(self.nakigoe)
+    def naku(self, arg=None):
+        if arg:
+            print(arg)
+        else:
+            print(self.nakigoe)
 
 neko = Animal("nya-n")
+neko.naku()
+neko.naku("wanwan")
 neko.naku()
