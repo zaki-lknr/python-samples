37d8520a4e548491206cec542420d8fd9122cd32
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-20 10:29:19 +0900

    add: モジュールからモジュールをimport

--- import/libs/subfunc.py
+++ import/libs/subfunc.py
@@ -1,3 +1,6 @@
+import libs.submain
 def spice():
     print("very spicy!!")
-    
+
+    # submain()  # NG; NameError: name 'submain' is not defined
+    libs.submain.submain()
---
+++ import/libs/submain.py
@@ -0,0 +1,3 @@
+def submain():
+    print("this is submain()")
+    
