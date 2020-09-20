8a21bc2d18f822c8988518d2500f8f2e1d4a6bca
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-20 10:37:44 +0900

    update: import -> from libs import submain

--- import/libs/subfunc.py
+++ import/libs/subfunc.py
@@ -1,6 +1,8 @@
-import libs.submain
+# import libs.submain
+from libs import submain
 def spice():
     print("very spicy!!")
 
     # submain()  # NG; NameError: name 'submain' is not defined
-    libs.submain.submain()
+    # libs.submain.submain()
+    submain.submain()
