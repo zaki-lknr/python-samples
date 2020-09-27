d59d0528f763d5eb7bcae67ca44e37fec1ea230b
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-27 23:03:41 +0900

    add: ヒアドキュメントで変数参照

--- here-document/sample.py
+++ here-document/sample.py
@@ -1,11 +1,15 @@
 if __name__ == '__main__':
     print("begin")
+    value1 = "カレー"
+    value2 = "パスタ"
 
     print("""
 foo
+{}
 bar
+{}
 baz
-    """)
+    """.format(value1, value2))
 
     print("end")
     
