1e3e260f49ed442f858f974c478f57fce4929cac
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-09-29 21:53:45 +0900

    add: 名前付きキャプチャ (https://docs.python.org/ja/3/library/re.html)

--- regexp/named-regexp.py
+++ regexp/named-regexp.py
@@ -2,8 +2,12 @@ import re
 
 string = '/path/to/sample.txt'
 
-result = re.match(r'(.*)/([^\.]*)\.(.*)', string)
+result = re.match(r'(?P<path>.*)/(?P<file>[^\.]*)\.(?P<ext>.*)', string)
 if result:
     print("path: " + result.group(1))
     print("file: " + result.group(2))
     print("ext: " + result.group(3))
+    print("---")
+    print("path: " + result.group('path'))
+    print("file: " + result.group('file'))
+    print("ext: " + result.group('ext'))
