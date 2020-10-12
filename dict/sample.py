d39c291b68022646a569f760c8453a419b335a37
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-10-12 20:48:41 +0900

    add: `dict()`を使った辞書作成

--- dict/sample.py
+++ dict/sample.py
@@ -5,4 +5,13 @@ print("begin")
 dict1 = {"key1": "value1", "key2": "value2"}
 print(dict1)
 
+dict2 = dict(key1="value1", key2="value2")
+print(dict2)
+
+if dict1 == dict2:
+    # 辞書の一致判定の処理系は未チェックだけど、このコードだとtrueになる
+    print("equal")
+else:
+    print("not equal")
+
 print("end")
