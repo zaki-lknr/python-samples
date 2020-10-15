849b9a73d6028af9d6a5b323561d6b1c54266a39
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-10-15 20:35:52 +0900

    add: dictに項目追加と既存項目上書き

--- dict/sample.py
+++ dict/sample.py
@@ -14,4 +14,30 @@ if dict1 == dict2:
 else:
     print("not equal")
 
+# add item to dict
+print("--- add item to dict ---")
+dict1['key3'] = "value3"
+
+if dict1 == dict2:
+    print("equal")
+else:
+    # ここでは不一致
+    print("> not equal")
+
+dict2['key3'] = "value3"
+if dict1 == dict2:
+    # ここでは一致
+    print("> equal")
+else:
+    print("not equal")
+
+# 上書き
+dict1['key1'] = "hoge"
+if dict1 == dict2:
+    print("equal")
+else:
+    # ここでは一致
+    print("> not equal")
+    print(dict1['key1'])
+
 print("end")
