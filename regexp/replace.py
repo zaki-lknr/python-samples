db1bba4d438d72552e633f39b139cc3162812764
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-11-29 16:29:04 +0900

    add: 正規表現メモリで置換

--- regexp/replace.py
+++ regexp/replace.py
@@ -6,3 +6,11 @@ string = '/path/to/sample.txt'
 string = re.sub(r'a', 'A', string)
 print(string)
 
+# 末尾の.txtを削る
+string = re.sub(r'\.txt$', '', string)
+print(string)
+
+# 't'と'p'を=t=か=p=に変換
+string = re.sub(r'([tp])', r'(\1)', string)
+print(string)
+# r'...'を使わない場合は'\\1'と書く
