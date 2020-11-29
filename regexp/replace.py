bca4af9bdba22110d24b34e9a4700ec64fc9eb71
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-11-29 16:59:37 +0900

    add: 大文字小文字無視でパターン置換

--- regexp/replace.py
+++ regexp/replace.py
@@ -14,3 +14,8 @@ print(string)
 string = re.sub(r'([tp])', r'(\1)', string)
 print(string)
 # r'...'を使わない場合は'\\1'と書く
+
+# aを変換。ただし大文字小文字無視
+string = re.sub(r'a', r'_', string, flags=re.IGNORECASE)
+print(string)
+# re.sub(pattern, repl, string, count=0, flags=0) なので、countを省略するためには"flags="という名前付き引数を使う
