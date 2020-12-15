f83929a79667259ad142e8575d502ce5094e51ec
zaki-lknr <zaki.hmkc+github@gmail.com>
2020-12-15 11:51:18 +0900

    add: logging()おためし

---
+++ logging/sample.py
@@ -0,0 +1,10 @@
+import logging
+
+if __name__ == '__main__':
+    print("hello world")
+
+    logging.critical('これはcritical')
+    logging.error('これはerror')
+    logging.warning('これはwarning')
+    logging.info('これはinfo')
+    logging.debug('これはdebug')
