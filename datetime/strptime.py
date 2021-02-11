c0dbf0264508a4748853936cdc8932305ac8349e
zaki-lknr <zaki.hmkc+github@gmail.com>
2021-02-11 20:51:16 +0900

    add: strptimeで"+0900"形式のタイムゾーン処理

--- datetime/strptime.py
+++ datetime/strptime.py
@@ -12,3 +12,9 @@ format_str_tz = '%a %b %d %H:%M:%S %Z %Y'
 dt_tz = datetime.datetime.strptime(date_str_tz, format_str_tz)
 print(dt_tz)
 print(dt_tz.tzinfo)  # None
+
+date_str_tz = 'Thu Feb 11 17:21:52 +0900 2021'
+format_str_tz = '%a %b %d %H:%M:%S %z %Y'
+dt_tz = datetime.datetime.strptime(date_str_tz, format_str_tz)
+print(dt_tz)
+print(dt_tz.tzinfo)  # None
