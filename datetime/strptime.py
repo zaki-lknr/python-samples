c898eed6b5d13d11a78533b9256f906118697574
zaki-lknr <zaki.hmkc+github@gmail.com>
2021-02-11 17:19:14 +0900

    add: タイムゾーン(JST)込みの文字列をstrptimeで文字列変換(tzinfoには反映されない)

--- datetime/strptime.py
+++ datetime/strptime.py
@@ -2,5 +2,13 @@ import datetime
 
 date_str = 'Thu Feb 11 17:01:34 2021'
 format_str = '%a %b %d %H:%M:%S %Y'
-datetime = datetime.datetime.strptime(date_str, format_str)
-print(datetime)
+dt = datetime.datetime.strptime(date_str, format_str)
+print(dt)
+print(dt.tzinfo)
+
+
+date_str_tz = 'Thu Feb 11 17:07:53 JST 2021'
+format_str_tz = '%a %b %d %H:%M:%S %Z %Y'
+dt_tz = datetime.datetime.strptime(date_str_tz, format_str_tz)
+print(dt_tz)
+print(dt_tz.tzinfo)  # None
