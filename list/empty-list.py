a9a0b650c47ffc8f31b5ba2123bf45d5ad4e7193
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-05-07 20:55:18 +0900

    add: empty list

---
+++ list/empty-list.py
@@ -0,0 +1,14 @@
+sample_array = [1,2,3]
+
+print(sample_array)
+print(type(sample_array))
+
+empty_array = []
+
+if empty_array == []:
+    print("blank")
+
+if empty_array:
+    print('true')
+else:
+    print('false')
