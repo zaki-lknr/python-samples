c16efb31b659cfc37408593c8ae3ce99198f0aaf
zaki-lknr <zaki.hmkc+github@gmail.com>
2025-08-10 10:42:09 +0900

    add: packageサンプル

---
+++ package-sample/my_package/__init__.py
@@ -0,0 +1,3 @@
+from .source import my_function
+
+# __all__ = ['my_function']
---
+++ package-sample/zzz.py
@@ -0,0 +1,7 @@
+from my_package import my_function
+
+my_function()
+
+# import my_package
+
+# my_package.my_function()
