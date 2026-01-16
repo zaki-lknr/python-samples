4631747dcc120081aed8495ba9d7979a173d4fcc
zaki-lknr <zaki.hmkc+github@gmail.com>
2026-01-16 17:07:46 +0900

    add: open(a+)

--- fileio/readwrite.py
+++ fileio/readwrite.py
@@ -1,6 +1,7 @@
-file_path = './sample.txt'
+file_path_wp = './sample1.txt'
+file_path_ap = './sample2.txt'
 
-with open(file_path, 'w+') as f:
+with open(file_path_wp, 'w+') as f:
     # w+ はread/write可能
     # ただしopen()の時点で既存ファイルは空
     data = f.read()
@@ -13,3 +14,11 @@ with open(file_path, 'w+') as f:
     data = f.read()
 
     print(data)
+
+with open(file_path_ap, 'a+') as f:
+    f.seek(0)
+    data = f.read()
+    print(data)
+
+    f.truncate(0)
+    f.write("bbb")
